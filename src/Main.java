8a6059b76e299658f068c4ffc9c76e349785dbe9
lucena <gabriel.lucena@ifood.com.br>
2022-11-12 15:51:37 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import br.com.dio.desafio.dominio.Curso;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        System.out.println(curso1);
     }
 }
